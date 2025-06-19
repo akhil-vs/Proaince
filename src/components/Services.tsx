d50b769f4569e837b18e7d6549fad034e72e7fff
akhil-vs <59328433+akhil-vs@users.noreply.github.com>
2025-06-19 18:29:39 +0100

    Removed duplicated industries we serve

---
+++ src/components/Services.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from '../pages/services/Header';
+import Banner from '../pages/services/Banner';
+import Banner2 from '../pages/services/Banner2';
+import Industries from '../pages/services/Industries';
+import Accordion from '../pages/services/Accordion';
+import WhyProaince from '../pages/services/WhyProaince';
+import Collab from '../pages/services/Collab';
+import IndustriesWeServe from '../pages/services/IndustriesWeServe';
+import StrategicPartners from '../pages/services/StrategicPartners';
+
+const Services = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Industries />
+      <Banner2 />
+      <Accordion />
+      <WhyProaince />
+      <Collab />
+      <StrategicPartners />
+    </div>
+  )
+}
+
+export default Services;
